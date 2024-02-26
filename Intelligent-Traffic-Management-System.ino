361bdcffc761bce68f2c9789089682b4d10fd49a
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-02-26 22:44:04 +0530

    Initialized variables to store sensor data

---
+++ Intelligent-Traffic-Management-System.ino
@@ -0,0 +1,48 @@
+#define road1RedLedPin    1
+#define road1GreenLedPin  2
+#define road1YellowLedPin 3
+
+#define road2RedLedPin    4
+#define road2GreenLedPin  5
+#define road2YellowLedPin 6
+
+#define road3RedLedPin    7
+#define road3GreenLedPin  8
+#define road3YellowLedPin 9
+
+#define road4RedLedPin    10
+#define road4GreenLedPin  11
+#define road4YellowLedPin 12
+
+#define YellowLightDuration=5
+
+#define lowDensityDuration=10
+#define mediumDensityDuration=20
+#define largeDensityDuration=30
+
+int road1MinUltraSensor=0;
+int road1MaxUltraSensor=0;
+
+int road2MinUltraSensor=0;
+int road2MaxUltraSensor=0;
+
+int road3MinUltraSensor=0;
+int road3MaxUltraSensor=0;
+
+int road4MinUltraSensor=0;
+int road4MaxUltraSensor=0;
+
+int road1RfidSensor=0;
+int road2RfidSensor=0;
+int road3RfidSensor=0;
+int road4RfidSensor=0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
